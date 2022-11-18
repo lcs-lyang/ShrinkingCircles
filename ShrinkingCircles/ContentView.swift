8b40e2ec1c6f95e18be62a10e2ca350b8fd06c26
Lillian Yang <lyang@lcs.on.ca>
2022-11-18 15:02:53 -0500

    Added circles manually

--- ShrinkingCircles/ContentView.swift
+++ ShrinkingCircles/ContentView.swift
@@ -13,6 +13,21 @@ struct ShrinkingCircle: Shape {
         //Create Path
         var path = Path()
         
+//        //First Circle
+//        path.addEllipse(in: CGRect(origin: CGPoint(x: rect.midX - rect.midY + 25 * 0, y: 0 + 25 * 0), size: CGSize(width: rect.height, height: rect.height)))
+//
+//        //Second Circle
+//        path.addEllipse(in: CGRect(origin: CGPoint(x: rect.midX - rect.midY + 25 * 1, y: 25 * 1), size: CGSize(width: rect.height - 50 * 1, height: rect.height - 50 * 1)))
+//
+//        //Third Circle
+//        path.addEllipse(in: CGRect(origin: CGPoint(x: rect.midX - rect.midY + 25 * 2, y: 25 * 2), size: CGSize(width: rect.height - 50 * 2, height: rect.height - 50 * 2)))
+//
+//        //Fourth Circle
+//        path.addEllipse(in: CGRect(origin: CGPoint(x: rect.midX - rect.midY + 25 * 3, y: 25 * 3), size: CGSize(width: rect.height - 50 * 3, height: rect.height - 50 * 3)))
+        
+        
+        
+        
         //Return the Path
         return path
         
@@ -25,7 +40,8 @@ struct ShrinkingCircle: Shape {
 struct ContentView: View {
     var body: some View {
         
-        Text("")
+        ShrinkingCircle()
+            .stroke()
         
     }
 }
